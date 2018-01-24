fcd6d12d05b02780ac2e4767a61ec4e9c6122a75
toomelette <toomelette@gmail.com>
2018-01-24 14:25:58 +0800

    Database Config

--- app/BurProjects.php
+++ app/BurProjects.php
@@ -4,8 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class BurProjects extends Model
-{
+class BurProjects extends Model{
     protected $table = 'bur_projects';
     public $timestamps = false;
 }
